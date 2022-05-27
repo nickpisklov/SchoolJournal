fd55fe48c70eb97822946a8f8ec33e9871e18f4e
nickpisklov <mykola.pisklov@nure.ua>
2022-05-27 16:04:53 +0300

    SJ-9 Authorization

---
+++ SchoolJournal/Controllers/AuthorizationController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SchoolJournal.Models;
+using System.Data.SqlClient;
+
+namespace SchoolJournal.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private IConfiguration configuration;
+
+        public AuthorizationController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ViewResult Index(User user)
+        {
+            bool isTeacher = user.TeacherStatusCheck(configuration);
+            bool IsStudent = user.StudentStatusCheck(configuration);
+
+            if (isTeacher == false && IsStudent == false)
+            {
+                ViewBag.IsGuest = true;
+                return View("Index", user);
+            }
+            else if (isTeacher == true && IsStudent == false)
+            {
+                ViewBag.IsGuest = false;
+                ViewBag.Status = "'Вчитель'";
+                //Set fields by login
+                return View("Index", user);
+            }
+            else 
+            {
+                ViewBag.IsGuest = false;
+                ViewBag.Status = "'Учень'";
+                //Set fields by login
+                return View("Index", user);
+            }
+        }
+    }
+}
