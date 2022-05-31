8a0218204e0f73f741fa4ba98e98cfcadda2cc4a
Daniel <63228639+Kinad404@users.noreply.github.com>
2022-05-31 19:55:48 +0300

    Update

---
+++ SchoolJournal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolJournal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
