4d99f54c67f0deac140847a33e0b52ec045229a7
nickpisklov <mykola.pisklov@nure.ua>
2022-06-11 18:20:03 +0300

    Editing homework and more proper status check

--- SchoolJournal/Models/Lesson.cs
+++ SchoolJournal/Models/Lesson.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SchoolJournal.Models
@@ -29,6 +30,10 @@ namespace SchoolJournal.Models
         {
             return db.Lessons.Where(l => l.FkClass == classId && l.FkSchoolYear == schoolYearId
                 && l.FkSubject == subjectId && l.FkTeacher == teacherId);
+        }
+        public static Lesson GetLessonById(SchoolJournalContext db, int lessonId) 
+        {
+            return db.Lessons.Where(l => l.Id == lessonId).FirstOrDefault();
         }
     }
 }
